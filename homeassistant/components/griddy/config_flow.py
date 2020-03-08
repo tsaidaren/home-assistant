010916b5a60a36e35875a3bea849cfca1de0fa07
J. Nick Koston <nick@koston.org>
2020-03-08 20:14:54 +0000

    adj

--- homeassistant/components/griddy/config_flow.py
+++ homeassistant/components/griddy/config_flow.py
@@ -44,14 +44,15 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             try:
                 info = await validate_input(self.hass, user_input)
-                await self.async_set_unique_id(user_input[CONF_LOADZONE])
-                self._abort_if_unique_id_configured()
-                return self.async_create_entry(title=info["title"], data=user_input)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
+
+            await self.async_set_unique_id(user_input[CONF_LOADZONE])
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(title=info["title"], data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
