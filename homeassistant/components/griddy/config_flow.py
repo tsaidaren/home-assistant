73d90f3266e7ce2d70fc19255dbeaf3e62bb0240
J. Nick Koston <nick@koston.org>
2020-03-08 20:08:48 +0000

    adj

--- homeassistant/components/griddy/config_flow.py
+++ homeassistant/components/griddy/config_flow.py
@@ -45,6 +45,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             try:
                 info = await validate_input(self.hass, user_input)
                 await self.async_set_unique_id(user_input[CONF_LOADZONE])
+                self._abort_if_unique_id_configured()
                 return self.async_create_entry(title=info["title"], data=user_input)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
