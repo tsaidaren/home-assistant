b38e0c185f687180a6033e2119f72eae32891e03
J. Nick Koston <nick@koston.org>
2020-03-25 04:11:04 +0000

    prefix is optional

--- homeassistant/components/elkm1/config_flow.py
+++ homeassistant/components/elkm1/config_flow.py
@@ -31,7 +31,7 @@ DATA_SCHEMA = vol.Schema(
         vol.Required(CONF_ADDRESS): str,
         vol.Optional(CONF_USERNAME, default=""): str,
         vol.Optional(CONF_PASSWORD, default=""): str,
-        vol.Required(CONF_PREFIX, default=""): str,
+        vol.Optional(CONF_PREFIX, default=""): str,
         vol.Optional(CONF_TEMPERATURE_UNIT, default="F"): vol.In(["F", "C"]),
     }
 )
