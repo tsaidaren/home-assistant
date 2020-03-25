677f42c6df5767198b3c293b59dabcfcd7c055ee
J. Nick Koston <nick@koston.org>
2020-03-25 17:25:05 +0000

    Adjust

--- homeassistant/components/recorder/__init__.py
+++ homeassistant/components/recorder/__init__.py
@@ -353,11 +353,13 @@ class Recorder(threading.Thread):
                 self.queue.task_done()
                 continue
             if event.event_type == EVENT_TIME_CHANGED:
+                _LOGGER.info("EVENT TIME CHANGED")
                 self.queue.task_done()
                 if self.commit_interval:
                     self._timechanges_seen += 1
                     if self.commit_interval >= self._timechanges_seen:
                         self._timechanges_seen = 0
+                        _LOGGER.info("DO COMMIT")
                         self._commit_event_session_or_retry()
                 continue
             if event.event_type in self.exclude_t:
