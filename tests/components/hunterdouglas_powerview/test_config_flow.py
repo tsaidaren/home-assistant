80a1b4f9d7f31952448d6caf9c7b142aef971b3b
J. Nick Koston <nick@koston.org>
2020-04-28 02:51:52 +0000

    more tests

--- tests/components/hunterdouglas_powerview/test_config_flow.py
+++ tests/components/hunterdouglas_powerview/test_config_flow.py
@@ -50,6 +50,17 @@ async def test_user_form(hass):
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
 
+    result3 = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result3["type"] == "form"
+    assert result3["errors"] == {}
+
+    result4 = await hass.config_entries.flow.async_configure(
+        result3["flow_id"], {"host": "1.2.3.4"},
+    )
+    assert result4["type"] == "abort"
+
 
 async def test_form_import(hass):
     """Test we get the form with import source."""
@@ -130,6 +141,17 @@ async def test_form_homekit(hass):
     assert len(mock_setup.mock_calls) == 1
     assert len(mock_setup_entry.mock_calls) == 1
 
+    result3 = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": "homekit"},
+        data={
+            "host": "1.2.3.4",
+            "properties": {"id": "AA::BB::CC::DD::EE::FF"},
+            "name": "PowerViewHub._hap._tcp.local.",
+        },
+    )
+    assert result3["type"] == "abort"
+
 
 async def test_form_cannot_connect(hass):
     """Test we handle cannot connect error."""
@@ -150,3 +172,43 @@ async def test_form_cannot_connect(hass):
 
     assert result2["type"] == "form"
     assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_no_ip(hass):
+    """Test we handle no ip being returned from the hub."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    mock_powerview_hub = _get_mock_powerview_hub()
+    with patch(
+        "homeassistant.components.hunterdouglas_powerview.config_flow.Hub",
+        return_value=mock_powerview_hub,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {"host": "1.2.3.4"},
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "cannot_connect"}
+
+
+async def test_form_unknown_exception(hass):
+    """Test we handle unknown exception."""
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+
+    mock_powerview_hub = _get_mock_powerview_hub(
+        ip="1.2.3.4", query_user_data=ValueError
+    )
+    with patch(
+        "homeassistant.components.hunterdouglas_powerview.config_flow.Hub",
+        return_value=mock_powerview_hub,
+    ):
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"], {"host": "1.2.3.4"},
+        )
+
+    assert result2["type"] == "form"
+    assert result2["errors"] == {"base": "unknown"}
