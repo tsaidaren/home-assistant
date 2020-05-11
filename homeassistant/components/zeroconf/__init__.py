72de8725952fa8b418e00b1448d6d312c4f78c0a
J. Nick Koston <nick@koston.org>
2020-05-11 19:49:56 +0000

    wip

--- homeassistant/components/zeroconf/__init__.py
+++ homeassistant/components/zeroconf/__init__.py
@@ -5,6 +5,8 @@ import socket
 
 import voluptuous as vol
 from zeroconf import (
+    DNSPointer,
+    DNSRecord,
     InterfaceChoice,
     NonUniqueNameException,
     ServiceBrowser,
@@ -73,6 +75,15 @@ def _get_instance(hass, default_interface=False):
     hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, stop_zeroconf)
 
     return zeroconf
+
+
+class HaServiceBrowser(ServiceBrowser):
+    """ServiceBrowser that only consumes DNSPointer records."""
+
+    def update_record(self, zc: "Zeroconf", now: float, record: DNSRecord) -> None:
+        if record.name != self.type or not isinstance(record, DNSPointer):
+            return
+        super().update_record(zc, now, record)
 
 
 class HaZeroconf(Zeroconf):
@@ -166,10 +177,10 @@ def setup(hass, config):
             )
 
     for service in ZEROCONF:
-        ServiceBrowser(zeroconf, service, handlers=[service_update])
+        HaServiceBrowser(zeroconf, service, handlers=[service_update])
 
     if HOMEKIT_TYPE not in ZEROCONF:
-        ServiceBrowser(zeroconf, HOMEKIT_TYPE, handlers=[service_update])
+        HaServiceBrowser(zeroconf, HOMEKIT_TYPE, handlers=[service_update])
 
     return True
 
