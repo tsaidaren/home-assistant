02de6f84152ffbb0f73a36cd3cdf65fd53e6f702
J. Nick Koston <nick@koston.org>
2020-05-11 22:21:52 +0000

    adj

--- homeassistant/block_async_io.py
+++ homeassistant/block_async_io.py
@@ -1,5 +1,6 @@
 """Block I/O being done in asyncio."""
 from http.client import HTTPConnection
+from ssl import SSLContext
 
 from homeassistant.util.async_ import protect_loop
 
@@ -8,6 +9,7 @@ def enable() -> None:
     """Enable the detection of I/O in the event loop."""
     # Prevent urllib3 and requests doing I/O in event loop
     HTTPConnection.putrequest = protect_loop(HTTPConnection.putrequest)
+    SSLContext.load_default_certs = protect_loop(SSLContext.load_default_certs)
 
     # Currently disabled. pytz doing I/O when getting timezone.
     # Prevent files being opened inside the event loop
