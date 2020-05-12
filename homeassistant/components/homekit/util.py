47eddc1c70850dabfdc7de26b88c43057c42e916
J. Nick Koston <nick@koston.org>
2020-05-12 22:57:10 +0000

    adjust

--- homeassistant/components/homekit/util.py
+++ homeassistant/components/homekit/util.py
@@ -484,7 +484,7 @@ def pid_is_alive(pid):
         # processes so we have to check waitpid
         # here or processes will be zombies even
         # after ffmpeg ends which will make
-        # os.kill(pid, 0) thing its still alive
+        # os.kill(pid, 0) think its still alive
         # because it has not been waited
         os.waitpid(pid, os.WNOHANG)
         os.kill(pid, 0)
