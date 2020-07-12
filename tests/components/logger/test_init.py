2c9aacfec755f848215114253817b6f35bc96255
J. Nick Koston <nick@koston.org>
2020-07-12 16:17:40 +0000

    add logger test

--- tests/components/logger/test_init.py
+++ tests/components/logger/test_init.py
@@ -1,5 +1,6 @@
 """The tests for the Logger component."""
 from collections import defaultdict
+import logging
 
 from homeassistant.components import logger
 from homeassistant.setup import async_setup_component
@@ -26,6 +27,7 @@ async def test_setting_level(hass):
                 }
             },
         )
+        await hass.async_block_till_done()
 
     assert len(mocks) == 4
 
@@ -72,4 +74,117 @@ async def test_setting_level(hass):
     assert len(mocks["new_logger"].setLevel.mock_calls) == 1
     assert (
         mocks["new_logger"].setLevel.mock_calls[0][1][0] == logger.LOGSEVERITY["NOTSET"]
+    )
+
+
+async def test_loading_integration_after_can_set_level(hass):
+    """Test logger propagation."""
+    assert await async_setup_component(hass, "history", {})
+    await hass.async_block_till_done()
+
+    assert await async_setup_component(
+        hass,
+        "logger",
+        {
+            "logger": {"default": "critical"},
+            "logs": {
+                "homeassistant": "warning",
+                "homeassistant.components": "info",
+                "homeassistant.components.logbook": "debug",
+                "homeassistant.components.history": "info",
+            },
+        },
+    )
+    await hass.async_block_till_done()
+    assert await async_setup_component(hass, "logbook", {})
+    await hass.async_block_till_done()
+
+    assert logging.getLogger("").isEnabledFor(logging.DEBUG) is False
+    assert logging.getLogger("").isEnabledFor(logging.CRITICAL) is True
+
+    assert logging.getLogger("homeassistant").isEnabledFor(logging.DEBUG) is False
+    assert logging.getLogger("homeassistant").isEnabledFor(logging.WARNING) is True
+
+    assert (
+        logging.getLogger("homeassistant.components").isEnabledFor(logging.DEBUG)
+        is False
+    )
+    assert (
+        logging.getLogger("homeassistant.components").isEnabledFor(logging.WARNING)
+        is True
+    )
+    assert (
+        logging.getLogger("homeassistant.components").isEnabledFor(logging.INFO) is True
+    )
+
+    assert (
+        logging.getLogger("homeassistant.components.logbook").isEnabledFor(
+            logging.DEBUG
+        )
+        is True
+    )
+    assert (
+        logging.getLogger("homeassistant.components.logbook").isEnabledFor(
+            logging.WARNING
+        )
+        is True
+    )
+    assert (
+        logging.getLogger("homeassistant.components.logbook").isEnabledFor(logging.INFO)
+        is True
+    )
+
+    assert (
+        logging.getLogger("homeassistant.components.logbook.unset").isEnabledFor(
+            logging.DEBUG
+        )
+        is True
+    )
+    assert (
+        logging.getLogger("homeassistant.components.logbook.unset").isEnabledFor(
+            logging.WARNING
+        )
+        is True
+    )
+    assert (
+        logging.getLogger("homeassistant.components.logbook.unset").isEnabledFor(
+            logging.INFO
+        )
+        is True
+    )
+
+    assert (
+        logging.getLogger("homeassistant.components.history").isEnabledFor(
+            logging.DEBUG
+        )
+        is False
+    )
+    assert (
+        logging.getLogger("homeassistant.components.history").isEnabledFor(
+            logging.WARNING
+        )
+        is True
+    )
+    assert (
+        logging.getLogger("homeassistant.components.history").isEnabledFor(logging.INFO)
+        is True
+    )
+
+    assert (
+        logging.getLogger("homeassistant.components.history.unset").isEnabledFor(
+            logging.DEBUG
+        )
+        is False
+    )
+    assert (
+        logging.getLogger("homeassistant.components.history.unset").isEnabledFor(
+            logging.WARNING
+        )
+        is True
+    )
+    assert (
+        logging.getLogger("homeassistant.components.history.unset").isEnabledFor(
+            logging.INFO
+        )
+        is True
     )
