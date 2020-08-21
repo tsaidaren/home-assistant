35b44f4ebbb0cd4792c580aea21a545960bf0464
J. Nick Koston <nick@koston.org>
2020-08-21 15:09:52 -0500

    convert emulated_hue to asyncio

--- homeassistant/components/emulated_hue/upnp.py
+++ homeassistant/components/emulated_hue/upnp.py
@@ -1,4 +1,5 @@
 """Support UPNP discovery method that mimics Hue hubs."""
+import asyncio
 import logging
 import select
 import socket
@@ -12,6 +13,9 @@ from homeassistant.components.http import HomeAssistantView
 from .const import HUE_SERIAL_NUMBER, HUE_UUID
 
 _LOGGER = logging.getLogger(__name__)
+
+BROADCAST_PORT = 1900
+BROADCAST_ADDR = "239.255.255.250"
 
 
 class DescriptionXmlView(HomeAssistantView):
@@ -51,6 +55,65 @@ class DescriptionXmlView(HomeAssistantView):
 """
 
         return web.Response(text=resp_text, content_type="text/xml")
+
+
+class UPNPResponderProtocol:
+    def __init__(self, loop, ssdp_socket, advertise_ip, advertise_port):
+        self.transport = None
+        self._loop = loop
+        self._sock = ssdp_socket
+        self.advertise_ip = advertise_ip
+        self.advertise_port = advertise_port
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        _LOGGER.error(f"Received {data!r} from {addr!r}")
+        data = f"I received {data!r}".encode("ascii")
+        # Don't close the socket as we might get multiple responses.
+
+    def error_received(self, exc):
+        _LOGGER.error("Error received: %s", exc)
+
+    def close(self):
+        self._loop.remove_writer(self._sock.fileno())
+        self._sock.close()
+
+
+@core.callback
+def create_upnp_datagram_endpoint(
+    host_ip_addr, upnp_bind_multicast, advertise_ip, advertise_port,
+):
+
+    # Listen for UDP port 1900 packets sent to SSDP multicast address
+    ssdp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ssdp_socket.setblocking(False)
+
+    # Required for receiving multicast
+    ssdp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    ssdp_socket.setsockopt(
+        socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(host_ip_addr)
+    )
+
+    ssdp_socket.setsockopt(
+        socket.SOL_IP,
+        socket.IP_ADD_MEMBERSHIP,
+        socket.inet_aton(BROADCAST_ADDR) + socket.inet_aton(host_ip_addr),
+    )
+
+    if upnp_bind_multicast:
+        ssdp_socket.bind(("", BROADCAST_PORT))
+    else:
+        ssdp_socket.bind((host_ip_addr, BROADCAST_PORT))
+
+    loop = asyncio.get_event_loop()
+
+    return loop.create_datagram_endpoint(
+        lambda: UPNPResponderProtocol(loop, ssdp_socket, advertise_ip, advertise_port),
+        sock=ssdp_socket,
+    )
 
 
 class UPNPResponderThread(threading.Thread):
@@ -107,11 +170,7 @@ class UPNPResponderThread(threading.Thread):
                 return
 
             try:
-                read, _, xcept = select.select([ssdp_socket], [], [ssdp_socket], 2)
-
-                _LOGGER.error("UPNP READ: %s", read)
-
-                _LOGGER.error("UPNP ERROR: %s", xcept)
+                read, _, _ = select.select([ssdp_socket], [], [ssdp_socket], 2)
 
                 if ssdp_socket in read:
                     data, addr = ssdp_socket.recvfrom(1024)
