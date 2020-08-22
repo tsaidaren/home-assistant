fa29f5d1be2c0238081dc25fa469f83620dc0415
J. Nick Koston <nick@koston.org>
2020-08-22 09:18:51 -0500

    Fix over-reaching mocking in metoffice

--- tests/components/metoffice/__init__.py
+++ tests/components/metoffice/__init__.py
@@ -1,1 +1,12 @@
 """Tests for the metoffice component."""
+
+import datetime
+
+
+class NewDateTime(datetime.datetime):
+    """Patch time to a specific point."""
+
+    @classmethod
+    def now(cls, *args, **kwargs):
+        """Overload datetime.datetime.now."""
+        return cls(2020, 4, 25, 12, tzinfo=datetime.timezone.utc)
