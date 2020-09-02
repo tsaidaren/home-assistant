9a19accce2db3792714e5960dfcbb3ce05fc2ebc
J. Nick Koston <nick@koston.org>
2020-09-01 20:43:34 -0500

    wip

--- homeassistant/components/time_interval/sensor.py
+++ homeassistant/components/time_interval/sensor.py
@@ -56,7 +56,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
             )
         )
 
-    async_add_entities(entities, True)
+    async_add_entities(entities)
 
 
 class TimeIntervalSensor(Entity):
