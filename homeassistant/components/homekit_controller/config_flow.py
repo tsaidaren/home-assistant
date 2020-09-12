27be1ce74976bdf848f27b2fefc1bfae277c2a99
J. Nick Koston <nick@koston.org>
2020-09-12 17:59:35 -0500

    adj

--- homeassistant/components/homekit_controller/config_flow.py
+++ homeassistant/components/homekit_controller/config_flow.py
@@ -98,6 +98,8 @@ class HomekitControllerFlowHandler(config_entries.ConfigFlow):
             await self.async_set_unique_id(
                 normalize_hkid(self.hkid), raise_on_progress=False
             )
+            # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
+            self.context["title_placeholders"] = {"model": self.model}
             return await self.async_step_pair()
 
         if self.controller is None:
@@ -217,6 +219,7 @@ class HomekitControllerFlowHandler(config_entries.ConfigFlow):
 
         # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
         self.context["hkid"] = hkid
+        self.context["title_placeholders"] = {"name": name, "model": self.model}
 
         if paired:
             # Device is paired but not to us - ignore it
@@ -350,9 +353,6 @@ class HomekitControllerFlowHandler(config_entries.ConfigFlow):
 
     @callback
     def _async_step_pair_show_form(self, errors=None):
-        # pylint: disable=no-member # https://github.com/PyCQA/pylint/issues/3167
-        self.context["title_placeholders"] = {"model": self.model}
-
         return self.async_show_form(
             step_id="pair",
             errors=errors or {},
