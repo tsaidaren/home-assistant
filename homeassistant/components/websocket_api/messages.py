76aa24259796741edaf9cefb0bb222e35a39ff29
J. Nick Koston <nick@koston.org>
2020-09-22 07:47:34 -0500

    log

--- homeassistant/components/websocket_api/messages.py
+++ homeassistant/components/websocket_api/messages.py
@@ -1,17 +1,21 @@
 """Message templates for websocket commands."""
 
 from functools import lru_cache
-import json
+import logging
 from typing import Any, Dict
 
 import voluptuous as vol
 
 from homeassistant.core import Event
 from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.json import JSONEncoder
+from homeassistant.util.json import (
+    find_paths_unserializable_data,
+    format_unserializable_data,
+)
 
 from . import const
 
+_LOGGER = logging.getLogger(__name__)
 # mypy: allow-untyped-defs
 
 # Minimal requirements of a message
@@ -24,12 +28,12 @@ MINIMAL_MESSAGE_SCHEMA = vol.Schema(
 BASE_COMMAND_MESSAGE_SCHEMA = vol.Schema({vol.Required("id"): cv.positive_int})
 
 
-def result_message(iden, result=None):
+def result_message(iden: int, result: Any = None) -> Dict:
     """Return a success result message."""
     return {"id": iden, "type": const.TYPE_RESULT, "success": True, "result": result}
 
 
-def error_message(iden, code, message):
+def error_message(iden: int, code: str, message: str) -> Dict:
     """Return an error result message."""
     return {
         "id": iden,
@@ -54,4 +58,22 @@ def cached_event_message(iden: int, event: Event) -> str:
     all getting many of the same events (mostly state changed)
     we can avoid serializing the same data for each connection.
     """
-    return json.dumps(event_message(iden, event), cls=JSONEncoder, allow_nan=False)
+    return message_to_json(event_message(iden, event))
+
+
+def message_to_json(message: Any) -> str:
+    """Serialize a websocket message to json."""
+    try:
+        return const.JSON_DUMP(message)
+    except (ValueError, TypeError):
+        _LOGGER.error(
+            "Unable to serialize to JSON. Bad data found at %s",
+            format_unserializable_data(
+                find_paths_unserializable_data(message, dump=const.JSON_DUMP)
+            ),
+        )
+        return const.JSON_DUMP(
+            error_message(
+                message["id"], const.ERR_UNKNOWN_ERROR, "Invalid JSON in response"
+            )
+        )
