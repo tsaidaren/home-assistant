87daf0bf791719abc89c66ca3b5710eb487be0b2
J. Nick Koston <nick@koston.org>
2020-09-23 21:37:33 -0500

    Revert "Revert "Revert "301"""
    This reverts commit b7993e328c502a6474ebe065f19c3e731aee9d2f.

--- homeassistant/components/logbook/__init__.py
+++ homeassistant/components/logbook/__init__.py
@@ -1,5 +1,4 @@
 """Event parser and human readable log generator."""
-import asyncio
 from datetime import timedelta
 from itertools import groupby
 import json
@@ -257,14 +256,6 @@ class LogbookView(HomeAssistantView):
                 )
             )
 
-        import cProfile
-
-        pr = cProfile.Profile()
-        pr.enable()
-        await asyncio.sleep(300)
-        pr.disable()
-        pr.create_stats()
-        pr.dump_stats("301.cprof")
         return await hass.async_add_executor_job(json_events)
 
 
