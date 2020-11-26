e5e77a7fb90adf6d0ea9196c2447bdff6f975072
J. Nick Koston <nick@koston.org>
2020-11-26 09:46:53 -1000

    adj

---
+++ alembic/versions/7c819a20d3bd_version_9.py
@@ -0,0 +1,24 @@
+"""Version 9
+
+Revision ID: 7c819a20d3bd
+Revises: 
+Create Date: 2020-11-26 09:39:36.427912
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '7c819a20d3bd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
