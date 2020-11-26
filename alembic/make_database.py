ba5e67026d32422b3ad6de144b8b40dc32a3eb43
J. Nick Koston <nick@koston.org>
2020-11-26 09:38:54 -1000

    adj

--- alembic/make_database.py
+++ alembic/make_database.py
@@ -1,6 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 
 from homeassistant.components.recorder.models import Base
+
+if os.path.exists("alembic/homeassistant-v2.db"):
+    os.unlink("alembic/homeassistant-v2.db")
 
 engine = create_engine("sqlite:///alembic/homeassistant-v2.db")
 Base.metadata.create_all(engine)
