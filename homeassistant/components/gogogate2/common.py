674cf2c3f7f9f280dc3961e0f4d4747a37ffb423
J. Nick Koston <nick@koston.org>
2021-03-01 17:37:47 -1000

    Use shared httpx client in gogogate2

--- homeassistant/components/gogogate2/common.py
+++ homeassistant/components/gogogate2/common.py
@@ -15,6 +15,7 @@ from homeassistant.const import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.debounce import Debouncer
+from homeassistant.helpers.httpx_client import get_async_client
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
@@ -108,7 +109,7 @@ def get_data_update_coordinator(
     config_entry_data = hass.data[DOMAIN][config_entry.entry_id]
 
     if DATA_UPDATE_COORDINATOR not in config_entry_data:
-        api = get_api(config_entry.data)
+        api = get_api(hass, config_entry.data)
 
         async def async_update_data():
             try:
@@ -137,7 +138,7 @@ def cover_unique_id(config_entry: ConfigEntry, door: AbstractDoor) -> str:
     return f"{config_entry.unique_id}_{door.door_id}"
 
 
-def get_api(config_data: dict) -> AbstractGateApi:
+def get_api(hass: HomeAssistant, config_data: dict) -> AbstractGateApi:
     """Get an api object for config data."""
     gate_class = GogoGate2Api
 
@@ -148,4 +149,5 @@ def get_api(config_data: dict) -> AbstractGateApi:
         config_data[CONF_IP_ADDRESS],
         config_data[CONF_USERNAME],
         config_data[CONF_PASSWORD],
+        httpx_async_client=get_async_client(hass),
     )
